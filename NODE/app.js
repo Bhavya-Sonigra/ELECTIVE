114537cbcba61bc575a4962cd9995d0120c22276
Bhavya Sonigra <sonigrabhavya28@gmail.com>
2025-07-04 14:02:35 +0530

    created secure server on university network using node server

--- NODE/app.js
+++ NODE/app.js
@@ -1,13 +1,16 @@
 // File: app.js
-const http = require('http');
+const https = require('https');
+const fs = require('fs');
 
-// Create a simple HTTP server
-// that responds with "Hello, World!" to all requests
-http.createServer((req, res) => {
-    // Set the response HTTP header with HTTP status and content type
+// Load the SSL certificate and key from files
+const options = {
+    key: fs.readFileSync('/Users/bhavyasonigra/Desktop/ELECTIVE/NODE/certs/key.pem'),  // Use fs to read the key
+    cert: fs.readFileSync('/Users/bhavyasonigra/Desktop/ELECTIVE/NODE/certs/cert.pem')  // Use fs to read the cert
+};
+https.createServer(options, (req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    // Send the response body "Hello, World!"
-    res.end('Hello, World!\n');
-}).listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+    res.end('Hello\n');
+    //Make
+}).listen(9080, '192.168.44.72', () => {
+    console.log('Server is running on https://192.168.44.72:9080');
 });
