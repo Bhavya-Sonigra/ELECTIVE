fc886841cb8d85d102a6ad264db06ccf965af70b
Bhavya Sonigra <sonigrabhavya28@gmail.com>
2025-07-11 12:19:15 +0530

    Updated the app.js

--- NODE/app.js
+++ NODE/app.js
@@ -2,11 +2,10 @@
 const https = require('https');
 const fs = require('fs');
 
-// Load the SSL certificate and key from files
 const options = {
     key: fs.readFileSync('/Users/bhavyasonigra/Desktop/ELECTIVE/NODE/certs/key.pem'),  // Use fs to read the key
     cert: fs.readFileSync('/Users/bhavyasonigra/Desktop/ELECTIVE/NODE/certs/cert.pem')  // Use fs to read the cert
-};
+}
 https.createServer(options, (req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello\n');
